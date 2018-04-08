62bbdb4bd5d7e2d9846d8f3d600e3f4bbd065391
Andrey Morozkin <andrey.dmitrievich@gmail.com>
2018-04-09 01:27:18 +0300

    bugfix in phpunit.bootstrap.php

--- tests/phpunit.bootstrap.php
+++ tests/phpunit.bootstrap.php
@@ -1,3 +1,11 @@
 <?php
-ini_set('assert.exception', 1);
+/**
+ * This file is part of the package moro/container7
+ *
+ * @see https://github.com/Moro4125/container7
+ * @license http://opensource.org/licenses/MIT
+ * @author Morozkin Andrey <andrey.dmitrievich@gmail.com>
+ */
+
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__.'/unit/_bootstrap.php';
